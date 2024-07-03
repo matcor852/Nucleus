35a3f060503a5bc89f9e31f7ae15b7c477946d3c
Matthieu Correia <matthieu.correia@epita.fr>
2024-07-03 02:46:19 +0200

    feat: keyboard layout; installer splash

--- debian/build.sh
+++ debian/build.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 
-# ISO
+if [ "$EUID" -ne 0 ]; then
+    echo "Must run as root."
+    exit 1
+fi
 
-```bash
-cd debian
-lb clean
+sudo lb clean
 lb config
+sudo lb build
 qemu-system-x86_64 -enable-kvm -cpu host -m 2048 -smp 4 -drive media=cdrom,format=raw,file=live-image-amd64.iso -boot d
-```
 
