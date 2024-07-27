bf6a4a51e749342b4e9a343767cc385590669c61
Matthieu Correia <matthieu.correia@epita.fr>
2024-07-27 19:16:06 +0200

    feat: discord updater

--- debian/config/includes.chroot/etc/skel/.config/discord/discord_renew.sh
+++ debian/config/includes.chroot/etc/skel/.config/discord/discord_renew.sh
@@ -4,5 +4,6 @@ set -e
 
 url='https://discord.com/api/download?platform=linux&format=deb'
 curl -L -o /tmp/discord.deb "$url"
-sudo apt install /tmp/discord.deb
+sudo apt-get install -y /tmp/discord.deb
+
 
